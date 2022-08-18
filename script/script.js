95a482e3f6bc39659b33bb1172329aee3b9c5cd3
Darktaste <vasil.lyubenov@gmail.com>
2022-08-18 22:44:05 +0300

    populating calc screen, AC and del button

--- script/script.js
+++ script/script.js
@@ -1,11 +1,21 @@
 const screenResult = document.querySelector(".result");
 const showOperation = document.querySelector(".showOperation");
 const numberSelection = document.querySelectorAll('[data-number]');
-const operation = document.querySelectorAll('[data-operand]');
-const clearButton = document.querySelector('[data-delete]');
+const operands = document.querySelectorAll('[data-operand]');
+const clearButton = document.querySelector('[data-clear]');
 const equalsButton = document.querySelector('[data-equals]');
 const delButton = document.querySelector('[data-delete]');
+let operand = undefined;
+screenResult.textContent = "0";
+showOperation.textContent = "0";
+let number ="";
 
+numberSelection.forEach((button) => {
+    button.addEventListener('click', () => populateScreen(button.textContent));
+})
+
+clearButton.addEventListener('click', clear);
+delButton.addEventListener('click', deleteNumber);
 
 function add(a, b) {
     return a + b;
@@ -16,7 +26,7 @@ function substract(a, b) {
 }
 
 function multiply(a, b) {
-     return a * b;
+    return a * b;
 }
 
 function divide(a, b) {
@@ -31,8 +41,8 @@ function power(a) {
     return a = Math.pow(a);
 }
 
-function operation(a, b ,operand) {
-    switch(operand){
+function operation(a, b, operand) {
+    switch (operand) {
         case "+":
             add(a, b);
             break;
@@ -54,3 +64,46 @@ function operation(a, b ,operand) {
     }
 }
 
+function clear() {
+    a = 0;
+    b = 0;
+    operand = undefined;
+    screenResult.textContent = "0";
+    showOperation.textContent = "0";
+    number = "";
+    removeLowerCase();
+}
+
+function populateScreen(number) {
+
+    if(screenResult.textContent === "0") {
+        screenResult.textContent = "";
+    }
+    
+    screenResult.textContent += number; 
+    addLowerCase();
+    
+
+}
+
+function deleteNumber() {
+    screenResult.textContent = screenResult.textContent.slice(0, -1);
+    if(screenResult.textContent === "") {
+        screenResult.textContent = "0";
+    }
+   removeLowerCase()
+}
+
+function removeLowerCase() {
+    if(screenResult.textContent.length <= 13){
+        screenResult.classList.remove("lower-case");
+
+    }
+}
+
+function addLowerCase() {
+    if(screenResult.textContent.length > 13){
+        screenResult.classList.add("lower-case");
+
+    }
+}
