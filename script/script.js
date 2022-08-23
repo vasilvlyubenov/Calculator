17ce2c69e1d5bb9f3c7fdf2cae3596268034108d
Darktaste <vasil.lyubenov@gmail.com>
2022-08-23 21:35:26 +0300

    working without keyboard input

--- script/script.js
+++ script/script.js
@@ -5,17 +5,26 @@ const operands = document.querySelectorAll('[data-operand]');
 const clearButton = document.querySelector('[data-clear]');
 const equalsButton = document.querySelector('[data-equals]');
 const delButton = document.querySelector('[data-delete]');
-let operand = undefined;
+const decimalPoint = document.querySelector('[data-decimal]');
+let operand = null;
 screenResult.textContent = "0";
 showOperation.textContent = "0";
-let number = "";
+let currentNumber = "";
+let prevNumber = "";
+let isDecimal = false;
 
 numberSelection.forEach((button) => {
     button.addEventListener('click', () => populateScreen(button.textContent));
 })
 
+operands.forEach((oButton) => {
+    oButton.addEventListener('click', () => setOpetation(oButton.textContent));
+})
+
 clearButton.addEventListener('click', clear);
 delButton.addEventListener('click', deleteNumber);
+equalsButton.addEventListener('click', evaluate);
+decimalPoint.addEventListener('click', checkDecimal);
 
 function add(a, b) {
     return a + b;
@@ -38,7 +47,7 @@ function percent(a) {
 }
 
 function power(a) {
-    return a = Math.pow(a);
+    return a = Math.pow(a, 2);
 }
 
 function operation(a, b, operand) {
@@ -47,31 +56,49 @@ function operation(a, b, operand) {
     b = Number(b);
     switch (operand) {
         case "+":
-            add(a, b);
-            break;
+            return add(a, b);
         case "-":
-            substract(a, b);
-            break;
+            return substract(a, b);
+
         case "*":
-            multiply(a, b);
-            break;
+            return multiply(a, b);
+
         case "/":
-            divide(a, b);
-            break;
+
+            if (b === 0) {
+                return null
+            }
+            else {
+                return divide(a, b);
+            }
+
+        default:
+            return null;
+
+    }
+}
+
+function singleDigitOperations(a, operand) {
+    a = Number(a);
+
+    switch (operand) {
+
         case "%":
-            percent(a);
-            break;
+            return percent(a);
+
         case "x²":
-            power(a);
-            break;
+            return power(a);
+        default: 
+            return null;
     }
 }
 
 function clear() {
-    operand = undefined;
+    operand = null;
     screenResult.textContent = "0";
     showOperation.textContent = "0";
-    number = "";
+    currentNumber = "";
+    prevNumber = "";
     removeLowerCase();
 }
 
@@ -79,8 +106,8 @@ function populateScreen(number) {
 
     if (screenResult.textContent === "0" && number !== ".") {
         screenResult.textContent = "";
-    } 
-
+    }
+    
     screenResult.textContent += number;
     addLowerCase();
     checkNumberLength(screenResult);
@@ -111,4 +138,59 @@ function checkNumberLength(numberLength) {
         alert("Cannot have more than 15 symbols");
         screenResult.textContent = screenResult.textContent.slice(0, -1);
     }
+}
+
+function checkDecimal() {
+    if(screenResult.textContent.includes('.')) return
+    screenResult.textContent += '.';
+}
+
+function setOpetation(sign) {
+
+    if (operand !== null) {
+        evaluate()
+    }
+
+    prevNumber = screenResult.textContent;
+        operand = sign;
+        screenResult.textContent = "0";
+
+    if (operand === "%") {
+        showOperation.textContent = `${prevNumber} / 100`;
+        evaluate();
+    } else if (operand === "x²") {
+        showOperation.textContent = `${prevNumber} ^ 2`;
+        evaluate();
+    }   else {
+        showOperation.textContent = `${prevNumber} ${operand}`;
+    }
+}
+
+function evaluate() {
+    if (screenResult.textContent === "0" && operand === "/") {
+        alert("You cannot divide by 0");
+        return
+    }
+    if (operand === null) return;
+
+    currentNumber = screenResult.textContent;
+    
+
+    if (operand === "%") {
+        showOperation.textContent = `${prevNumber} / 100 =`;
+        screenResult.textContent = formatResult(singleDigitOperations(prevNumber, operand));
+    } else if (operand === "x²") {
+        showOperation.textContent = `${prevNumber} ^ 2 =`;
+        screenResult.textContent = formatResult(singleDigitOperations(prevNumber, operand));
+    }   else {
+        showOperation.textContent = `${prevNumber} ${operand} ${currentNumber} =`;
+        screenResult.textContent = formatResult(operation(prevNumber, currentNumber, operand));
+    }
+
+    operand = null
+
+}
+
+function formatResult(number) {
+    return Math.round(number * 1000) / 1000;
 }
